e6d81e5a8a86db8599aa439bcb5bb3cb4016fc4a
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-12-16 09:43:12 -0600

    shears.sh: support fixup! and squash!
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- share/msysGit/shears.sh
+++ share/msysGit/shears.sh
@@ -278,6 +278,38 @@ exec git update-ref refs/rewritten/$commit HEAD\\
 ")"
 	done
 
+	lastline=9999
+	while true
+	do
+		fixup="$(echo "$todo" |
+			sed "$lastline,\$d" |
+			grep -n -e '^pick [^ ]* \(fixup\|squash\)!' |
+			tail -n 1)"
+		test -n "$fixup" || break
+
+		linenumber=${fixup%%:*}
+		oneline="${fixup#* }"
+		shortsha1="${oneline%% *}"
+		oneline="${oneline#* }"
+		command=${oneline%%!*}
+		oneline="${oneline#*! }"
+		oneline_regex="^pick [^ ]* $(echo "$oneline" |
+			sed 's/[\\\/*?]/\\&/g')\$"
+		targetline="$(echo "$todo" |
+			sed "$linenumber,\$d" |
+			grep -n "$oneline_regex" |
+			tail -n 1)"
+		targetline=${targetline%%:*}
+		if test -n "$targetline"
+		then
+			todo="$(echo "$todo" |
+				sed -e "${linenumber}d" \
+					-e "${targetline}a\\
+$command $shortsha1 $oneline")"
+		fi
+		lastline=$(($linenumber+1))
+	done
+
 	todo="$(printf '%s\n\n%s' "$todo" "cleanup $needslabel")"
 	echo "$todo" | uniq
 }
